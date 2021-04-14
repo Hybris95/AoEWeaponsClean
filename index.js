caa82aaec3d93c6e71949b1067c0115184b81e08
Hybris95 <hybris_95@hotmail.com>
2021-04-14 20:31:08 +0200

    Added Warp support

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const { Setting, SettingsObject } = require("SettingsManager/SettingsManager.js")
 const Settings = new SettingsObject("AoEWeaponsClean", [
     {
-        name:"AoE Weapons Clean Toggles",
+        name:"Weapons Clean Toggles",
         settings: [
-            new Setting.Toggle("Weapon Messages", false),
-			new Setting.Toggle("Debug Messages", false)
+            new Setting.Toggle("AoE Messages", false),
+			new Setting.Toggle("Warp Messages", false)
         ]
-    }
-]).setCommand("aoewc").setSize(Renderer.screen.getWidth()/2, Renderer.screen.getHeight()/2);
+    },
+	{
+		name:"Weapons Clean Developer",
+		settings: [
+			new Setting.Toggle("Debug Messages", false)
+		]
+	}
+]).setCommand("wclean").setSize(Renderer.screen.getWidth()/2, Renderer.screen.getHeight()/2);
 Setting.register(Settings);
 
 register("chat", (weapon,nbTargets,multiply,damage,event) => {
-	if(Settings.getSetting("AoE Weapons Clean Toggles","Weapon Messages")){
-		if(Settings.getSetting("AoE Weapons Clean Toggles","Debug Messages")){
+	if(Settings.getSetting("Weapons Clean Toggles","AoE Messages")){
+		if(Settings.getSetting("Weapons Clean Developer","Debug Messages")){
 			ChatLib.chat("&cRemoving " + weapon + " message for " + nbTargets + " target(s) and " + damage + " damage");
 		}
 		cancel(event);
 	}
 }).setCriteria("Your ${weapon} hit ${nbTargets} enem${multiply} for ${damage} damage.")
+
+register("chat", (event) => {
+	if(Settings.getSetting("Weapons Clean Toggles","Warp Messages")){
+		if(Settings.getSetting("Weapons Clean Developer","Debug Messages")){
+			ChatLib.chat("&cRemoving warp message");
+		}
+		cancel(event);
+	}
+}).setCriteria("There are blocks in the way!")
